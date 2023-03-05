6897e358d2efd96e20440e56048fe5c9737fe8a9
jsagao <james.agao@gmail.com>
2023-03-04 19:55:04 -0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 //import logo from './logo.svg';
 import './App.css';
 // import Login from './Login/Login';
-// import Admin from './Admin/Admin';
+import Admin from './Admin/Admin';
 import Blog from './Blog/Blog';
 import Navbar from './components/Navbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
+import About from './Pages/About';
 // import About from './Pages/About';
 
 function App() {
   return (
     <>
       {/* <Login></Login> */}
-      {/* <Admin></Admin> */}
       {/* <Blog></Blog> */}
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Navbar />}>
             <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
             <Route path="blog" element={<Blog />} />
+            <Route path="admin" element={<Admin />} />
           </Route>
         </Routes>
 
